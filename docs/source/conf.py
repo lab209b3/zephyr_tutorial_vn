60393afeccfcfe92fd633b0a855dbed38aa8bc6f
baobuibk <buiqbao@gmail.com>
2024-03-14 22:18:42 +0700

    fix: duplicate reference index file
    The index.rst file contain duplicate entry, need to remove
    fix some warning about short underline
    remove the conf file in sub folder
    add tab extension in conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = 'Bao Bui'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_rtd_theme",]
+extensions = ["sphinx_rtd_theme",'sphinx_tabs.tabs']
 
 templates_path = ['_templates']
 exclude_patterns = []
